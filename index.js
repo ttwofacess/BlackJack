cb9783dd3559ce73ce95daadd188d38eb9250bf7
ttwofacess <20799318+ttwofacess@users.noreply.github.com>
2024-06-30 21:56:10 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+/* let firstCard = 6;
+let secondCard = 9; */
+let cards = [firstCard, secondCard];
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+let messageEl = document.getElementById("message-el");
+/* let sumEl = document.getElementById("sum-el"); */
+let sumEl = document.querySelector("#sum-el");
+let cardsEl = document.getElementById("cards-el");
+
+function startGame() {
+    renderGame();
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
+    sumEl.textContent = "Sum: " + sum;
+    if(sum <= 20) {
+        message = "Do you want to draw a new card?";
+    } else if(sum === 21) {
+        message = "You've got Blackjack!";
+        hasBlackJack = true;
+    } else {
+        message = "You're out of the game!";
+        isAlive = false;
+    }
+
+    /* console.log(message); */
+    messageEl.textContent = message;    
+}
+/* message = "we're logging out"; */
+
+function newCard() {
+    console.log("Drawing a new card from the deck!");
+    let card = 5;
+    sum += card;
+    renderGame();
+}
