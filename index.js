f2ae57feddb88f967275400e7253fe45ca54bed8
ttwofacess <20799318+ttwofacess@noreply.github.com>
2024-07-19 21:53:37 -0300

    feat: Implementing object with player data.

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 let secondCard = 9; */ 
 /* let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard; */
+let player = {
+    name: "Me",
+    chips: 200
+}
 let cards = [];
 let sum = 0;
 let hasBlackJack = false;
@@ -11,6 +15,9 @@ let messageEl = document.getElementById("message-el");
 /* let sumEl = document.getElementById("sum-el"); */
 let sumEl = document.querySelector("#sum-el");
 let cardsEl = document.getElementById("cards-el");
+let playerEl = document.getElementById("player-el");
+
+playerEl.textContent = player.name + ": $" + player.chips;
 
 function getRandomCard() {
     let randomNumber =  Math.floor(Math.random() * 13) + 1;
